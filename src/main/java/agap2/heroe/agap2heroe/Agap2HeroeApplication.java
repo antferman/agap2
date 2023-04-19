5565a1d90197566398fbd31455cb145cfccfa279
DESKTOP-1H6L6O9\nano3 <aferman993@gmail.com>
2023-04-19 18:00:24 +0200

    main - Initial project

---
+++ src/main/java/agap2/heroe/agap2heroe/Agap2HeroeApplication.java
@@ -0,0 +1,13 @@
+package agap2.heroe.agap2heroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agap2HeroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agap2HeroeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agap2/heroe/agap2heroe/Agap2HeroeApplicationTests.java
@@ -0,0 +1,13 @@
+package agap2.heroe.agap2heroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agap2HeroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
