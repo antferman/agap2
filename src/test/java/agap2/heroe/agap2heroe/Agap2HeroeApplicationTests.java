58436d4c23ceee51f2ac590d164f4210d98c8274
DESKTOP-1H6L6O9\nano3 <aferman993@gmail.com>
2023-04-20 22:58:31 +0200

    feature/Task001 added one test using mockito

--- src/test/java/agap2/heroe/agap2heroe/Agap2HeroeApplicationTests.java
+++ src/test/java/agap2/heroe/agap2heroe/Agap2HeroeApplicationTests.java
@@ -1,13 +1,40 @@
 package agap2.heroe.agap2heroe;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import agap2.heroe.model.Heroe;
+import agap2.heroe.service.HeroeService;
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class Agap2HeroeApplicationTests {
 
+	@LocalServerPort
+	private int port;
+	
+    @MockBean
+    private HeroeService service;
+
 	@Test
-	void contextLoads() {
+	void testGetAllHeroes() {
+		Heroe heroe = new Heroe(1, "Heroe1");
+		List<Heroe> list = new ArrayList<>();
+		list.add(heroe);
+	    when(service.getAllHeroes()).thenReturn(list);
+		RestAssured.given().get("http://localhost:"+port+"/heroe/getAll").then().assertThat()
+	      .statusCode(HttpStatus.OK.value()).body(containsString("Heroe1"));
 	}
 
 }
